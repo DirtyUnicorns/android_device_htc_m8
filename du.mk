bc630ef2054796befbfa83e63c6913fc52c4b37e
NYCHitman1 <jchasky@gmail.com>
2014-06-15 12:19:25 -0400

    DU bring up for M8

---
+++ du.mk
@@ -0,0 +1,41 @@
+#
+# Copyright 2012 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+ 
+# Sample: This is where we'd set a backup provider if we had one
+# $(call inherit-product, device/sample/products/backup_overlay.mk)
+
+# Get the long list of APNs
+PRODUCT_COPY_FILES := vendor/du/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/du/config/common.mk)
+$(call inherit-product, vendor/du/config/gsm.mk)
+
+# Inherit from hardware-specific part of the product configuration
+# Inherit from device-specific configurations
+$(call inherit-product, device/htc/m8/device.mk)
+
+# Inherit m8-specific vendor tree
+$(call inherit-product-if-exists, vendor/htc/m8/m8-vendor.mk)
+
+PRODUCT_NAME := du_m8
+PRODUCT_DEVICE := m8
+PRODUCT_BRAND := htc
+PRODUCT_MODEL := One M8
+PRODUCT_MANUFACTURER := HTC
