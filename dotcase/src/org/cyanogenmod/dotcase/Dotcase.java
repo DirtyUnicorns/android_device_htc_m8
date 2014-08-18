80721ba1d3be721295d32fad5556a4ca46f563ca
k5t4j5 <k_jones_ford@hotmail.com>
2014-08-18 12:14:30 -0400

    Fix Torch Constants/Dotcase
    Change-Id: I389a8c1b42a62df23b2281e260edfaa892dae90e

--- dotcase/src/org/cyanogenmod/dotcase/Dotcase.java
+++ dotcase/src/org/cyanogenmod/dotcase/Dotcase.java
@@ -43,7 +43,7 @@ import android.view.View;
 import android.view.WindowManager;
 
 import com.android.internal.telephony.ITelephony;
-import com.android.internal.util.cm.TorchConstants;
+import com.android.internal.util.du.TorchConstants;
 
 import java.lang.Math;
 import java.io.BufferedReader;
