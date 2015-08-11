1718a2ebaa892270c974a17c8ae1096c2ab3c5be
Alex Cruz <mazdarider23@gmail.com>
2015-08-11 09:06:13 +0000

    Fix back button on action bar not working

--- DeviceHandler/src/com/slim/device/settings/ScreenOffGestureSettings.java
+++ DeviceHandler/src/com/slim/device/settings/ScreenOffGestureSettings.java
@@ -39,4 +39,15 @@ public class ScreenOffGestureSettings extends Activity {
         getActionBar().setDisplayHomeAsUpEnabled(true);
 
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+            switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
