1cdd353a7422984535aa33ad196fe24bcd9bb5e7
Susan McCombs <smac0628@gmail.com>
2016-03-18 16:32:18 -0400

    Bluetooth fix
    Revert "bdroid: sync with bacon"
    
    This reverts commit c63f886a3169a534a2da60216e84d09f2ba5e79b.
    
    Change-Id: I0ed11e8fe8388cf56d9675718dc6bb2f45f36e0e

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -21,12 +21,10 @@
 
 #define BTM_DEF_LOCAL_NAME   "HTC One M8"
 // Disables read remote device feature
-#define BTA_SKIP_BLE_READ_REMOTE_FEAT FALSE
-#define MAX_L2CAP_CHANNELS    16
-#define BLE_VND_INCLUDED   TRUE
+#define BTA_SKIP_BLE_READ_REMOTE_FEAT TRUE
+#define MAX_L2CAP_CHANNELS    14
 // skips conn update at conn completion
-#define BTA_BLE_SKIP_CONN_UPD  FALSE
-#define BLE_PERIPHERAL_ADV_NAME  FALSE
-#define BT_CLEAN_TURN_ON_DISABLED 1
-
+#define BTA_BLE_SKIP_CONN_UPD  TRUE
+// Enables interleaved scan
+#define BTA_HOST_INTERLEAVE_SEARCH TRUE
 #endif
