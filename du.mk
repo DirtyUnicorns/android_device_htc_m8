e39209ae48001679ba3465314dea011495ec76b3
Susan M <smac0628@gmail.com>
2016-04-02 21:19:36 -0400

    DU-ify
    Change-Id: I7ef0424830743e32686651ec3266aec797eca4a5

--- du.mk
+++ du.mk
@@ -17,9 +17,9 @@
 $(call inherit-product, device/htc/m8/full_m8.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/du/config/nfc_enhanced.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
 
-PRODUCT_NAME := cm_m8
+PRODUCT_NAME := du_m8
